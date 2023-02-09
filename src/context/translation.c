317dcec6a01e2e0322e7bdac2fe515d21f8e157c
Anthony Tsang <htsang@student.42heilbronn.de>
2023-02-10 00:29:51 +0100

    refactor: moved translation.c inside the context module

--- src/context/translation.c
+++ src/context/translation.c
@@ -6,14 +6,13 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 23:28:11 by htsang            #+#    #+#             */
-/*   Updated: 2023/02/08 23:16:20 by htsang           ###   ########.fr       */
+/*   Updated: 2023/02/10 00:06:07 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol_context.h"
 
-int	translate_left_or_right(t_fractol_canvas *canvas, \
-t_fractol_painter *painter, t_fractol_func fractal, unsigned int controls)
+int	translate_left_or_right(t_fractol_context *program, unsigned int controls)
 {
 	if (!controls || controls == 0b0011)
 	{
@@ -21,20 +20,20 @@ t_fractol_painter *painter, t_fractol_func fractal, unsigned int controls)
 	}
 	if ((controls & TRANSLATE_LEFT) == TRANSLATE_LEFT)
 	{
-		copy_pixels_right(canvas->image, 20);
-		calculate_left_translation(canvas, 20);
+		copy_pixels_right(program->canvas.image, 20);
+		calculate_left_translation(&program->canvas, 20);
 	}
 	else
 	{
-		copy_pixels_left(canvas->image, 20);
-		calculate_right_translation(canvas, 20);
+		copy_pixels_left(program->canvas.image, 20);
+		calculate_right_translation(&program->canvas, 20);
 	}
-	paint_fractal(canvas, painter, fractal);
+	program->painter_func( \
+		&program->canvas, &program->painter, program->fractal);
 	return (1);
 }
 
-int	translate_up_or_down(t_fractol_canvas *canvas, \
-t_fractol_painter *painter, t_fractol_func fractal, unsigned int controls)
+int	translate_up_or_down(t_fractol_context *program, unsigned int controls)
 {
 	if (!controls || controls == 0b1100)
 	{
@@ -42,14 +41,24 @@ t_fractol_painter *painter, t_fractol_func fractal, unsigned int controls)
 	}
 	if ((controls & TRANSLATE_DOWN) == TRANSLATE_DOWN)
 	{
-		copy_pixels_up(canvas->image, 20);
-		calculate_down_translation(canvas, 20);
+		copy_pixels_up(program->canvas.image, 20);
+		calculate_down_translation(&program->canvas, 20);
 	}
 	else
 	{
-		copy_pixels_down(canvas->image, 20);
-		calculate_up_translation(canvas, 20);
+		copy_pixels_down(program->canvas.image, 20);
+		calculate_up_translation(&program->canvas, 20);
 	}
-	paint_fractal(canvas, painter, fractal);
+	program->painter_func( \
+		&program->canvas, &program->painter, program->fractal);
 	return (1);
+}
+
+int	translate(t_fractol_context *program)
+{
+	int	painted;
+
+	painted = translate_left_or_right(program, program->controls & 0b0011);
+	painted += translate_up_or_down(program, program->controls & 0b1100);
+	return (painted);
 }
