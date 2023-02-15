db1ba04145eb8aa1ed5b8fa63b89f7e91875e038
Anthony Tsang <htsang@student.42heilbronn.de>
2023-02-16 00:33:31 +0100

    feat: Added interface for distance_map, and an interface for a more detailed color control

--- src/canvas/canvas.c
+++ src/canvas/canvas.c
@@ -6,20 +6,11 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 21:46:24 by htsang            #+#    #+#             */
-/*   Updated: 2023/02/14 18:55:47 by htsang           ###   ########.fr       */
+/*   Updated: 2023/02/15 21:49:25 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FRACTOL/canvas.h"
-
-void	set_base_color(t_fractol_color_controls *color_controls, \
-uint8_t r, uint8_t g, uint8_t b)
-{
-	color_controls->r = r;
-	color_controls->g = g;
-	color_controls->b = b;
-	color_controls->potential_factor = 1000;
-}
 
 uint32_t	*get_pixel(t_fractol_canvas *canvas, uint32_t x, uint32_t y)
 {
--- src/canvas/canvas_setters.c
+++ src/canvas/canvas_setters.c
@@ -6,7 +6,7 @@
 /*   By: htsang <htsang@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/04 00:18:01 by htsang            #+#    #+#             */
-/*   Updated: 2023/02/14 16:53:46 by htsang           ###   ########.fr       */
+/*   Updated: 2023/02/15 22:42:19 by htsang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,26 @@ uint32_t start_x, uint32_t start_y)
 	canvas->start_x = start_x;
 	canvas->start_y = start_y;
 	return (canvas);
+}
+
+void	set_base_color(t_fractol_color_controls *color_controls, \
+uint8_t r, uint8_t g, uint8_t b)
+{
+	color_controls->r = r;
+	color_controls->g = g;
+	color_controls->b = b;
+}
+
+void	set_color_factor(t_fractol_color_controls *color_controls, \
+double r_factor, double g_factor, double b_factor)
+{
+	color_controls->r_factor = r_factor;
+	color_controls->g_factor = g_factor;
+	color_controls->b_factor = b_factor;
+}
+
+void	set_potential_factor(t_fractol_color_controls *color_controls, \
+int32_t potential_factor)
+{
+	color_controls->potential_factor = potential_factor;
 }
